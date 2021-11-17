4dddd4d77ec3ac851e1c946dbab83561f8e68746
motmarsda <emma.lindell.6567@student.uu.se>
2021-11-17 17:50:27 +0100

    no of burger buttons done

--- src/components/Burger.vue
+++ src/components/Burger.vue
@@ -1,5 +1,4 @@
 <template>
-<!-- "<div> {{ burger.name }} {{ burger.kCal }}</div>" -->
 
   <div class = "Burger" >
       <h3 v-bind:key="burger.name"> {{ burger.name }} </h3>
@@ -10,7 +9,16 @@
         <li v-bind:key="burger.content" > Contains <span class="allergy">{{ burger.content }}</span> </li>
         <li v-bind:key="burger.descript"> {{ burger.descript }} </li>
       </u1>
+      <button type="button" v-on:click="subFromOrder">
+          <img src="https://cdn3.vectorstock.com/i/1000x1000/13/82/left-arrow-icon-vector-21641382.jpg" style = "height:1em;">
+      </button>
+      <span> {{ amountOrdered }}</span>
+      <button type="buttom" v-on:click="addToOrder()">
+          <img src="https://cdn1.vectorstock.com/i/1000x1000/88/85/right-arrow-icon-vector-21638885.jpg" style = "height:1em;">
+      </button>
   </div>
+
+
 </template>
 
 <script>
@@ -18,8 +26,28 @@ export default {
   name: 'Burger',
   props: {
     burger: Object
+  },
+  data: function () {
+    return {
+      amountOrdered: 0,
+    }
+  },
+  methods: {
+    subFromOrder: function(){
+      if(this.amountOrdered>0){
+        this.amountOrdered -=1;
+      }
+      else{
+        this.amountOrdered =0;
+      }
+
+    },
+    addToOrder: function(){
+      this.amountOrdered +=1;
+    }
   }
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
